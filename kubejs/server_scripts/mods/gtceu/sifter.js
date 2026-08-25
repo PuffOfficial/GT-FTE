febd21d75b4d0afbede079f9d2c1c0272ab6278b
PuffOfficial <you@example.com>
2026-08-25 13:16:38 +0200

    Added 3 thousand ways to obtain sphalerite

--- kubejs/server_scripts/mods/gtceu/sifter.js
+++ kubejs/server_scripts/mods/gtceu/sifter.js
@@ -21,8 +21,10 @@ ServerEvents.recipes(event => {
       .EUt(eu);
   }
 
-  sifting(`2x minecraft:sand`, `minecraft:water 1000`, [`4x gtceu:impure_cassiterite_sand_dust`, `8x gtceu:impure_redstone_dust`, `2x gtceu:impure_sphalerite_dust`, `4x gtceu:impure_sulfur_dust`, `gtceu:crushed_emerald_ore`, `gtceu:raw_diamond`], 200, 1, `gravel_sifting`, GTValues.VA[GTValues.ULV])
-  sifting(`2x minecraft:gravel`, `minecraft:water 1000`, [`2x gtceu:crushed_copper_ore`, `2x gtceu:crushed_iron_ore`, `2x gtceu:crushed_gold_ore`, `2x gtceu:crushed_lead_ore`, `4x gtceu:raw_coal`, `gtceu:raw_sapphire`], 200, 1, `sand_sifting`, GTValues.VA[GTValues.ULV])
+  sifting(`2x minecraft:sand`, `minecraft:water 1000`, [`4x gtceu:impure_cassiterite_sand_dust`, `8x gtceu:impure_redstone_dust`, `2x gtceu:impure_sphalerite_dust`, `4x gtceu:impure_sulfur_dust`], 200, 1, `gravel_sifting`, GTValues.VA[GTValues.ULV])
+  sifting(`2x minecraft:gravel`, `minecraft:water 1000`, [`2x gtceu:crushed_copper_ore`, `2x gtceu:crushed_iron_ore`, `2x gtceu:crushed_gold_ore`, `2x gtceu:crushed_lead_ore`, `2x gtceu:crushed_sphalerite_ore`], 200, 1, `sand_sifting`, GTValues.VA[GTValues.ULV])
+  sifting(`4x minecraft:gravel`, `minecraft:water 1000`, [`4x gtceu:raw_coal`,`2x gtceu:raw_sapphire`, `gtceu:crushed_emerald_ore`,`gtceu:raw_diamond`], 500, 2, `gravel_sifting_rare`, GTValues.VA[GTValues.ULV])
+
 
   event.recipes.gtceu.sifter(`gtfte:gravel_sifting_fixed`)
     .itemInputs(`minecraft:gravel`).circuit(2)
