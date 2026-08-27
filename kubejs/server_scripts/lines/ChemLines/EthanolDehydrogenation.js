055191338350039adf91ce3bbb833fc784fdc652
PuffOfficial <you@example.com>
2026-08-27 19:25:42 +0200

    Rebalanced Ethanol Dehydrogenation line

--- kubejs/server_scripts/lines/ChemLines/EthanolDehydrogenation.js
+++ kubejs/server_scripts/lines/ChemLines/EthanolDehydrogenation.js
@@ -2,13 +2,13 @@ ServerEvents.recipes(event => {
 event.recipes.gtceu.chemical_reactor("gtfte:vanadium_pentoxide")
         .itemInputs("2x gtceu:vanadium_dust")
         .inputFluids("gtceu:oxygen 5000")
-        .itemOutputs("7x gtceu:vanadium_pentoxide_dust")
+        .itemOutputs("2x gtceu:vanadium_pentoxide_dust")
         .duration(300)
         .EUt(GTValues.VA[GTValues.LV]);
 event.recipes.gtceu.chemical_reactor("gtfte:molybdenum_trioxide")
         .itemInputs("gtceu:molybdenum_dust")
         .inputFluids("gtceu:oxygen 3000")
-        .itemOutputs("4x gtceu:molybdenum_trioxide_dust")
+        .itemOutputs("gtceu:molybdenum_trioxide_dust")
         .duration(300)
         .EUt(GTValues.VA[GTValues.LV]);
 
@@ -16,7 +16,8 @@ event.recipes.gtceu.chemical_reactor("gtfte:maleic_anhydride")
         .notConsumable("gtceu:vanadium_pentoxide_dust")
         .notConsumable("gtceu:molybdenum_trioxide_dust")
         .inputFluids(["gtceu:benzene 1000", "gtceu:air 3500"])
-        .outputFluids(["minecraft:water 4000", "gtceu:maleic_anhydride 1000"])
+        .outputFluids(["minecraft:water 1000", "gtceu:maleic_anhydride 1000"])
+        .itemOutputs(`2xgtceu:carbon_dust`)
         .duration(300)
         .EUt(GTValues.VA[GTValues.HV]);
 
@@ -31,7 +32,7 @@ event.recipes.gtceu.large_chemical_reactor("gtfte:butadiene_from_acetaldehyde")
         .notConsumable("10x gtceu:sapphire_dust")
         .notConsumable("5x gtceu:zincite_dust")
         .inputFluids(["gtceu:acetaldehyde 2000"])
-        .outputFluids(["gtceu:butadiene 1000", "minecraft:water 2000", "gtceu:carbon_dioxide 1000"])
+        .outputFluids(["gtceu:butadiene 1000", "minecraft:water 1000"])
         .duration(600)
         .EUt(GTValues.VA[GTValues.EV]);
 })
